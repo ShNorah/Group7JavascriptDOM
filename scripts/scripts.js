8ea9c839226b14595d1624d8a09168a71f033c7e
Ayesiza Hawah <yesiha2001@gmail.com>
2020-03-22 13:36:40 +0300

    change animation page using bootstrap

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,4 +8,7 @@ document.getElementById('catshate').innerHTML ="The second paragraph explains: "
  
 //getElementByTagName method used id = catshate to find the element
 var y = document.getElementsByTagName('ol');
-document.getElementById('catshate').innerHTML = "These are the thing cats hate: " + y[0].innerHTML;
+document.getElementById('catshate').innerHTML = "<strong>These are the thing cats hate:</strong> " + y[0].innerHTML;
+
+var z = document.getElementsByTagName('ul');
+document.getElementById('catslove').innerHTML = "These are the things cats love:" + z[0].innerHTML;
